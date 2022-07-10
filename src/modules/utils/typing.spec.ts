0b1b0b0f2b3c7d070f94c90633fdc1a327933658
Sg <suica@users.noreply.github.com>
2022-07-10 11:14:17 +0800

    test(typing): add test cases for typing utils

---
+++ src/modules/utils/typing.spec.ts
@@ -0,0 +1,29 @@
+import { assertNever, isNotNil } from "./typing";
+
+describe("assertNever", () => {
+  it("should throw error when invoked", () => {
+    const f = () => {
+      assertNever(1 as never);
+    };
+    expect(f).toThrowError();
+  });
+});
+
+describe("isNotNil", () => {
+  it("should work for nullish values", () => {
+    expect(isNotNil(null)).toBe(false);
+    expect(isNotNil(undefined)).toBe(false);
+  });
+
+  it("should work for falsy but not nullish values", () => {
+    expect(isNotNil(false)).toBe(true);
+    expect(isNotNil("")).toBe(true);
+    expect(isNotNil(0)).toBe(true);
+    expect(isNotNil(-0)).toBe(true);
+  });
+  it("should work for truthy values", () => {
+    expect(isNotNil(true)).toBe(true);
+    expect(isNotNil(1)).toBe(true);
+    expect(isNotNil("haha")).toBe(true);
+  });
+});
