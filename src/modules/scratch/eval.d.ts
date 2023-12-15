dd95a66eceefc0d6b55d3099292421bf8543da82
Sg <suica@users.noreply.github.com>
2023-12-15 16:14:43 +0800

    refactor: organize code

--- src/modules/scratch/eval.d.ts
+++ src/modules/scratch/eval.d.ts
@@ -17,6 +17,42 @@ import { EnsureArr, IsEmptyList, MatchCase } from './utils/helper';
 import { Add, EQUALS, Lt, Lte, Sub } from './utils/nat';
 
 type NatLiteralConcept = MakeValueExpr<NatConcept>;
+
+export type Eval<
+  env extends EnvConcept,
+  expr extends ExprConcept[] | ExprConcept,
+  _input extends ExprConcept[] = EnsureArr<expr>,
+  __result extends any = IsEmptyList<_input> extends true
+    ? // no stmt, do nothing
+      env
+    : // get first element to process
+    _input extends [
+        infer head extends ExprConcept,
+        ...infer tail extends ExprConcept[],
+      ]
+    ? // TODO: test "return" state and decided whether to stop ?
+      Eval<EvalSingleStmt<env, head>, tail>
+    : never,
+> = __result;
+
+export type EvalSingleStmt<
+  env extends EnvConcept,
+  expr extends ExprConcept,
+  __returns extends EnvConcept = expr extends BindExprConcept
+    ? UpdateEnv<env, [Omit<expr, 'kind'>], []>
+    : expr extends EmptyStmtConcept
+    ? env // do nothing
+    : expr extends BinaryExprConcept
+    ? EvalBinaryExpr<env, expr>
+    : expr extends IfStmtConcept
+    ? env
+    : expr extends ValueExprConcept
+    ? UpdateEnv<env, [], [expr['value']]>
+    : expr extends IdentifierConcept
+    ? UpdateEnv<env, [], [Lookup<env, expr['name']>]>
+    : never,
+> = __returns;
+
 type EvalBinaryExpr<
   env extends EnvConcept,
   expr extends BinaryExprConcept,
@@ -61,42 +97,7 @@ type TestEvalBinaryExpr = [
   >,
 ];
 
-export type Eval<
-  env extends EnvConcept,
-  expr extends ExprConcept[] | ExprConcept,
-  _input extends ExprConcept[] = EnsureArr<expr>,
-  __result extends any = IsEmptyList<_input> extends true
-    ? // no stmt, do nothing
-      env
-    : // get first element to process
-    _input extends [
-        infer head extends ExprConcept,
-        ...infer tail extends ExprConcept[],
-      ]
-    ? // TODO: test "return" state and decided whether to stop ?
-      Eval<EvalSingleStmt<env, head>, tail>
-    : never,
-> = __result;
-
-export type EvalSingleStmt<
-  env extends EnvConcept,
-  expr extends ExprConcept,
-  __returns extends EnvConcept = expr extends BindExprConcept
-    ? UpdateEnv<env, [Omit<expr, 'kind'>], []>
-    : expr extends EmptyStmtConcept
-    ? env // do nothing
-    : expr extends BinaryExprConcept
-    ? EvalBinaryExpr<env, expr>
-    : expr extends IfStmtConcept
-    ? env
-    : expr extends ValueExprConcept
-    ? UpdateEnv<env, [], [expr['value']]>
-    : expr extends IdentifierConcept
-    ? UpdateEnv<env, [], [Lookup<env, expr['name']>]>
-    : never,
-> = __returns;
-
-export type TestValSingleStmt = [
+export type TestEvalSingleStmt = [
   Expect<EQUALS<EvalSingleStmt<_SampleEnv, EmptyStmtConcept>, _SampleEnv>>,
   Expect<
     EQUALS<
