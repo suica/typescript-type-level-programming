9944200e2604b1ea321a796e1fd48ef2df524f1d
Sg <suica@users.noreply.github.com>
2023-12-20 14:42:26 +0800

    fix: format sample code

--- src/modules/playground/samples.ts
+++ src/modules/playground/samples.ts
@@ -1,5 +1,4 @@
-import { HKTWithArity, PartialApply } from "./hkt";
-import { $, Fixed, _0, _1 } from "./hkts";
+import { HKTWithArity, PartialApply } from './hkt';
 
 type Nat = Array<1>;
 type IsNotEmpty<a extends any[]> = a['length'] extends 0 ? false : true;
@@ -15,23 +14,23 @@ type One = [1];
 type LengthOfOne = One['length']; // 得到 1
 type Two = [1, 1];
 
-
 type BadFold<
   nums extends Nat[],
   f,
   acc extends Nat = [],
 > = IsNotEmpty<nums> extends true
-  //@ts-expect-error
-  ? BadFold<Tail<nums>, f, f<acc, Head<nums>>>
+  ? //@ts-expect-error
+    BadFold<Tail<nums>, f, f<acc, Head<nums>>>
   : acc;
 //@ts-expect-error
 type TestBadFold = BadFold<[One, Two], Add>;
 
-type AssertNat<T> = T extends Nat ? T : never;
-
 type Assert<T, P> = T extends P ? T : never;
 export interface AddHKT extends HKTWithArity<2> {
-  type: Add<Assert<this['TypeArguments'][0], Nat>, Assert<this['TypeArguments'][1], Nat>>;
+  type: Add<
+    Assert<this['TypeArguments'][0], Nat>,
+    Assert<this['TypeArguments'][1], Nat>
+  >;
 }
 
 type ShouldBeThree = PartialApply<AddHKT, [One, Two]>['length'];
